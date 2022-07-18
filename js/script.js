933c6d041df7bf85b1d0a9fee0920edfc49f33de
Vaishali Tyagi <89056569+vaishalicareer11@users.noreply.github.com>
2022-07-17 23:58:33 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let music = new Audio("music.mp3");
+let audioturn = new Audio("images/turn.wav");
+let gameover = new Audio("images/winning.mp3");
+let turn = "X";
+let isgameover = false;
+
+// function to change the turn
+const changeTurn = () =>{
+
+  return turn ==="X" ? "0":"X"
+}
+
+// function to check for a win
+const checkwin =()=>{
+  let boxtext = document.getElementsByClassName('boxtext');
+   let wins = [
+    [0,1,2, 5, 5, 0],
+    [3,4,5, 5, 5, 15],
+    [6,7,8, 5, 15, 0],
+    [0,3,6 , -5, 15, 90],
+    [1,4,7, 5, 15, 90],
+    [2,5,8, 15, 15, 90],
+    [0,4,8, 5, 15, 45],
+    [2,4,6, 5, 15, 135],
+   ]
+   wins.forEach(e =>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+      document.querySelector(".info").innerText = boxtext[e[0]].innerText + ` won`;
+      isgameover = true;
+      gameover.play();
+      document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+      document.querySelector('.line').style.width = `20vw`;
+      document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+    }
+   })
+}
+
+// game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener('click', ()=>{
+    if(boxtext.innerText===''){
+      boxtext.innerText=turn;
+     turn = changeTurn();
+      audioturn.play();
+      checkwin();
+      if(!isgameover){
+        document.getElementsByClassName('info')[0].innerText = `turn for ` + turn;
+      }
+    }
+  })
+})
+
+// reset tic tac toe 
+document.getElementById("reset").addEventListener('click', ()=>{
+  let boxtext = document.querySelectorAll(".boxtext");
+   Array.from(boxtext).forEach(element => {
+    element.innerText = "";
+   });
+   turn = "X";
+   isgameover = false;
+   document.getElementsByClassName('info')[0].innerText = `turn for ` + turn; 
+   document.querySelector('.line').style.width = `0vw`;
+   gameover.pause();
+   gameover.currentTime = 0;
+   document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0px";
+})
